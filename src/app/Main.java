04b1417a52c0d06e54acb9090bd94ac254759c8e
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-05-19 11:16:18 +0100

    Add a temperature converter from Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,21 @@
 package app;
 
+
 public class Main {
 
+    private static double CONV_F_C = 0;
+
+
     public static void main(String[] args) {
-        System.out.println("Converter °C to °F!!!");
+        System.out.println("My converter degrees Fahrenheit to Celsius.");
+
+        double F = 32;
+        double celsius = convFrToCe(F);
+        System.out.println("Result is " + celsius + "° celsius.");
+    }
+
+    private static double convFrToCe(double F) {
+        return CONV_F_C = ((F - 32) * 5) / 9;
+
     }
 }
